619f82ef1cf9e24a46021261e85b0e561e1cc310
Greycell <dominik.severin@yandex.com>
2023-02-14 13:33:44 +0100

    Working AlienShip Explosion

--- src/Shield.java
+++ src/Shield.java
@@ -4,6 +4,5 @@ import java.awt.*;
 public class Shield {
     public static Image shieldImage = new ImageIcon(Shield.class.getResource("/shield.png"))
             .getImage().getScaledInstance(145, 120, Image.SCALE_DEFAULT);
-    public static Rectangle shieldHitbox;
 
 }
